2ffb39bb785496b18b81efad639d85ed8ee95f49
Vitalii Drobiazko <vitalii.drobiazko@nixs.com>
2023-08-09 20:43:47 +0300

    Chenge script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,2 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 echo 'This is the first step within the scrit'
-=======
-echo 'This is the first step within the scrit'
->>>>>>> feature/stuff2
-=======
-echo 'This is the first step within the scrit'
->>>>>>> feature/stuff2
-=======
-echo 'This is the first step within the scrit'
->>>>>>> feature/stuff2
+
